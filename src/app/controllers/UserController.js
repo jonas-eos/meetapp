db48179651758da2118e596255690e5cbebb0817
Jonas Evangelista <jonas.eos@outlook.com>
2019-08-23 01:42:55 -0300

    chore: update user

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -47,6 +47,66 @@ class UserController {
       email,
     });
   }
+
+  async update(__request, __response) {
+    /**
+     * Setting fields properties
+     * Passowrd: required if oldPassowrd exist, min 6 character
+     * confirmPassword: required if Password and oldPassword exist
+     *    min 6 character.
+     */
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    /**
+     * Validate request body.
+     */
+    if (!(await schema.isValid(__request.body))) {
+      return __response.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { email, oldPassword } = __request.body;
+    const user = await User.findByPk(__request.userId);
+
+    /**
+     * Validate if email exist on body
+     * find user email on database
+     * return error if user email exist.
+     */
+    if (email && email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return __response.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    /**
+     * Validate Password
+     */
+    if (oldPassword && (await user.passwordCorrect(oldPassword)) === false) {
+      return __response.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, email: userEmail } = await user.update(__request.body);
+
+    return __response.json({
+      id,
+      name,
+      email: userEmail,
+    });
+  }
 }
 
 export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -8,5 +8,5 @@ const routes = new Router();
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
-
+routes.put('/users', UserController.update);
 export default routes;
