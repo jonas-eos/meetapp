c3449acd19f36f53e9ff8f0c4fb37c4a6b6e927c
Jonas Evangelista <jonas.eos@outlook.com>
2019-09-08 05:39:45 -0300

    chore: database config

--- src/config/database.js
+++ src/config/database.js
@@ -3,13 +3,17 @@
  *
  * @description
  * This file contains the database configurations.
+ *
+ * @requires dotenv/config
  */
+require('dotenv/config');
+
 module.exports = {
-  dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'admin',
-  database: 'meetapp',
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   define: {
     timestamps: true,
     underscored: true,
