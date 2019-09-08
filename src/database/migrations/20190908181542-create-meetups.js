c44be6814d47fe153c6067c512ea30b5334810a6
Jonas Evangelista <jonas.eos@outlook.com>
2019-09-08 15:34:52 -0300

    chore: meetups table created

---
+++ src/database/migrations/20190908181542-create-meetups.js
@@ -0,0 +1,76 @@
+/**
+ * @overview create table: meetups
+ *
+ * @description
+ * This file create user table with fields:
+ * ID: { Integer, autoIncrement, PK }
+ * organizer_id: { Integer, Relation with user:id }
+ * banner: { Integer, relation with files:id }
+ * title: { String }
+ * description: { Text }
+ * date: { date }
+ * created_at: { date }
+ * updated_at: { date }
+ *
+ * @require Sequelize
+ */
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('meetups', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      organizer_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      banner: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'files',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('meetups');
+  },
+};
